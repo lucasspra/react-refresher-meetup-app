c0f0f784be0879e9bc758aa977ae7da5fc68f2b4
Lucas Souza <lucas.souza@ifood.com.br>
2021-06-30 17:22:38 -0300

    Create Layout component

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,13 @@
+import MainNavigation from "./MainNavigation";
+import style from "./Layout.module.css";
+
+function Layout(props) {
+  return (
+    <div>
+      <MainNavigation />
+      <main className={style.main}>{props.children}</main>
+    </div>
+  );
+}
+
+export default Layout;
--- src/components/meetups/MeetupItem.jsx
+++ src/components/meetups/MeetupItem.jsx
@@ -1,19 +1,22 @@
+import Card from "../ui/Card";
 import style from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
   return (
     <li className={style.item}>
-      <div className={style.image}>
-        <img src={props.image} alt={props.title} />
-      </div>
-      <div className={style.content}>
-        <h3>{props.title}</h3>
-        <address>{props.address}</address>
-        <p>{props.description}</p>
-      </div>
-      <div className={style.actions}>
-        <button>Add to Favorites</button>
-      </div>
+      <Card>
+        <div className={style.image}>
+          <img src={props.image} alt={props.title} />
+        </div>
+        <div className={style.content}>
+          <h3>{props.title}</h3>
+          <address>{props.address}</address>
+          <p>{props.description}</p>
+        </div>
+        <div className={style.actions}>
+          <button>Add to Favorites</button>
+        </div>
+      </Card>
     </li>
   );
 }
