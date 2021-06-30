228cee311c66754f96eb52a2cd4a924ed608a680
Lucas Souza <lucas.souza@ifood.com.br>
2021-06-30 11:27:29 -0300

    Configure routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { Route, Switch } from "react-router";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetUp";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
