c4c0aedde35e31a7855b27af30f19d5c5bb40f92
Lucas Souza <lucas.souza@ifood.com.br>
2021-07-01 21:05:59 -0300

    Final commit

--- src/components/layout/MainNavigation.jsx
+++ src/components/layout/MainNavigation.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
-
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
 import style from "./MainNavigation.module.css";
 
 function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
     <header className={style.header}>
       <div className={style.logo}>React Meetups</div>
@@ -11,11 +14,16 @@ function MainNavigation() {
           <li>
             <Link to="/">All Meetups</Link>
           </li>
+
           <li>
             <Link to="/new-meetup">Add New Meetup</Link>
           </li>
+
           <li>
-            <Link to="/favorites">Favorites</Link>
+            <Link to="/favorites">
+              My Favorites
+              <span className={style.badge}>{favoritesCtx.totalFavorites}</span>
+            </Link>
           </li>
         </ul>
       </nav>
--- src/components/meetups/MeetupItem.jsx
+++ src/components/meetups/MeetupItem.jsx
@@ -1,20 +1,44 @@
+import { useContext } from "react";
+import FavoritesContext from "../../store/favorites-context";
 import Card from "../ui/Card";
 import style from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+  function toggleFavoriteStatusHandler() {
+    if (itemIsFavorite) {
+      favoritesCtx.removeFavorite(props.id);
+    } else {
+      favoritesCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address,
+      });
+    }
+  }
+
   return (
     <li className={style.item}>
       <Card>
         <div className={style.image}>
           <img src={props.image} alt={props.title} />
         </div>
+
         <div className={style.content}>
           <h3>{props.title}</h3>
           <address>{props.address}</address>
           <p>{props.description}</p>
         </div>
+
         <div className={style.actions}>
-          <button>Add to Favorites</button>
+          <button onClick={toggleFavoriteStatusHandler}>
+            {itemIsFavorite ? "Remove from Favorites" : "Add to Favorites"}
+          </button>
         </div>
       </Card>
     </li>
